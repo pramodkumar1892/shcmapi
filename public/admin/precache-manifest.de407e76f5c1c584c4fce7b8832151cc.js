bd9a530979940b5b69cec2c5463356f6afb4bb35
Pramod <pramod.kumar@sarvika.com>
2020-06-21 17:35:31 +0530

    webapp added

---
+++ public/admin/precache-manifest.de407e76f5c1c584c4fce7b8832151cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b286ed19acd76d69ae600995d884d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "685986d65aacadc56f58",
+    "url": "/static/css/main.29a29f94.chunk.css"
+  },
+  {
+    "revision": "76908a6b68a7d958fa2c",
+    "url": "/static/js/2.d5fd2fcc.chunk.js"
+  },
+  {
+    "revision": "c80b09f21c3dd2137be347c65d78f97d",
+    "url": "/static/js/2.d5fd2fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685986d65aacadc56f58",
+    "url": "/static/js/main.3b8a2bfc.chunk.js"
+  },
+  {
+    "revision": "9acfd0f391a41579b0c4",
+    "url": "/static/js/runtime-main.56050200.js"
+  },
+  {
+    "revision": "067a11f232da2b02352b867768995729",
+    "url": "/static/media/appicon.067a11f2.PNG"
+  }
+]);
